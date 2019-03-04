e558ec8d666cd8cd65c1c12b7aa54dedbb3abe10
Luke <lukembeck@gmail.com>
2019-03-04 23:15:14 +1000

    Updates

--- precache-manifest.2763e929c3801e468fc892649ccbe9a5.js
+++ precache-manifest.2763e929c3801e468fc892649ccbe9a5.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "3669deb7cebcbff369ab",
-    "url": "/kyukei/static/js/main.3669deb7.chunk.js"
+    "revision": "404ed47cb5abe4f1634b",
+    "url": "/kyukei/static/js/main.404ed47c.chunk.js"
   },
   {
     "revision": "94835c579f9e0f2bc4e9",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/kyukei/static/js/2.cdcfef16.chunk.js"
   },
   {
-    "revision": "9554a8fa53bcba55ee89bc8701060eed",
+    "revision": "78ec05df4d49080efe07e6c74a04d14e",
     "url": "/kyukei/index.html"
   }
 ];
